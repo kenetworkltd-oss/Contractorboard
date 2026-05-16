5c23fab7eba549234f7138b18c81292fc68009dd
Agboola Kehinde Oluwaseun~ <agboolakehinde167@gmail.com>
2026-05-16 15:40:21 +0100

    Fix duplicate navbar and update hero image

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,12 +4,12 @@ from allauth.socialaccount.models import SocialAccount
 from .models import User
 
 
-@receiver(post_save, sender=SocialAccount)
-def set_social_user_type(sender, instance, created, **kwargs):
-    if created:
-        user = instance.user
-        # Default Google signups to homeowner
-        # They can change later in settings
-        if not user.user_type:
-            user.user_type = User.HOMEOWNER
-            user.save()
+# @receiver(post_save, sender=SocialAccount)
+# def set_social_user_type(sender, instance, created, **kwargs):
+#     if created:
+#         user = instance.user
+#         # Default Google signups to homeowner
+#         # They can change later in settings
+#         if not user.user_type:
+#             user.user_type = User.HOMEOWNER
+#             user.save()
