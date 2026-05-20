061ac555e27e300b484f2426c138069711dacac0
Agboola Kehinde Oluwaseun~ <agboolakehinde167@gmail.com>
2026-05-20 05:30:02 +0100

    Fix homeowner registration 500 error

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import login
+# from .forms import ContractorRegistrationForm, HomeownerRegistrationForm
+# from .models import ContractorProfile, User
+
+
+# def register_contractor(request):
+#     if request.method == 'POST':
+#         form = ContractorRegistrationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.user_type = User.CONTRACTOR
+#             user.save()
+#             ContractorProfile.objects.create(
+#                 user=user,
+#                 business_name=form.cleaned_data['business_name'],
+#                 niche=form.cleaned_data['niche'],
+#                 service_area=form.cleaned_data['service_area'],
+#             )
+#             login(request, user)
+#             return redirect('dashboard')
+#     else:
+#         form = ContractorRegistrationForm()
+#     return render(request, 'accounts/register_contractor.html', {'form': form})
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import ContractorRegistrationForm, HomeownerRegistrationForm
